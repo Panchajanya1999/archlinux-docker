281c80706f82eae5be4c41cd1fbc3d38a4b69c69
Panchajanya Sarkar <panchajanya@azure-dev.live>
2020-05-03 12:33:13 +0000

    install: Split into a seperate executable file
    Signed-off-by: Panchajanya Sarkar <panchajanya@azure-dev.live>

---
+++ install_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Uncomment the community [multilib] repository
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+
+# Run pacman to update
+pacman -Syyu --noconfirm
+
+# Install basic packages
+pacman -S --noconfirm \
+                 wget  \
+                 cmake svn lzip git make patchelf zip \
+                 inetutils python2 lld llvm base-devel\
+                 clang bc ccache multilib-devel glibc \
+                 sudo 
