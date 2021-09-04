90324d3dedadb9f1335cddb026f71c0ec4763228
Panchajanya Sarkar <rsk52959@gmail.com>
2021-09-04 16:32:02 +0530

    install_deps: Install ncurses

--- install_deps.sh
+++ install_deps.sh
@@ -14,7 +14,7 @@ pacman -S --noconfirm \
                  inetutils python2 lld llvm base-devel\
                  clang bc ccache multilib-devel z3 \
                  sudo jdk8-openjdk bison cmake flex libelf \
-                 ninja openssl python3 uboot-tools neofetch
+                 ninja openssl python3 uboot-tools neofetch ncurses
 
 # Use glibc-2.33-4-x86
 curl -fsSL "https://repo.archlinuxcn.org/x86_64/glibc-linux4-2.33-4-x86_64.pkg.tar.zst" | bsdtar -C / -xvf - &>/dev/null
