4a5745e790f7d53fda8284c45b48b9c188e88d77
Panchajanya Sarkar <rsk52959@gmail.com>
2022-02-16 17:40:09 +0000

    Revert "install-deps: Use glibc-2.33-4"
    This reverts commit 8d8d9c440b2f95a4ebdcded3843cf593e5d07665.
    
    Signed-off-by: Panchajanya Sarkar <rsk52959@gmail.com>

--- install_deps.sh
+++ install_deps.sh
@@ -12,12 +12,9 @@ pacman -S --noconfirm \
                  python-sphinx python-sphinx_rtd_theme graphviz imagemagick \
                  cmake svn lzip git make patchelf zip \
                  inetutils python2 lld llvm base-devel\
-                 clang bc ccache multilib-devel z3 \
+                 clang bc ccache multilib-devel glibc z3 \
                  sudo jdk8-openjdk bison cmake flex libelf \
                  ninja openssl python3 uboot-tools neofetch ncurses
-
-# Use glibc-2.33-4-x86
-curl -fsSL "https://repo.archlinuxcn.org/x86_64/glibc-linux4-2.33-4-x86_64.pkg.tar.zst" | bsdtar -C / -xvf - &>/dev/null
 
 # Fix pod2man missing error
 export PATH=/usr/bin/core_perl:$PATH
